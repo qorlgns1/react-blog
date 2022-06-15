3b5a08e751456a5296d6d5196bfb97217bec0c9c
qorlgns1 <rlgns0610@gmail.com>
2022-06-15 20:20:00 +0900

    feat: 페이지 구조를 Header, Banner, Home, Footer로 분리하고 라우터 설정
    Heaeder, Banner, Footer 는 공통적으로 사용할 것이고, Home은 유동적으로 바꾸면서 사용할 계획을 가지고 분리하였습니다.
    그리고 라우터 path에 맞지않는 경로 입력 시 페이지를 찾을 수 없다고 나올 수 있게 만들었습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import Home from "./pages/home/Home";
+import Header from "./components/layouts/header/Header";
+import Footer from "./components/layouts/footer/Footer";
+import Banner from "./components/layouts/banner/Banner";
+import { BrowserRouter } from "react-router-dom";
+import { Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
+
 function App() {
-	return <>React</>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Banner />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login">
+          <div>Login Page</div>
+        </Route>
+        <Route path="/register">
+          <div>Register Page</div>
+        </Route>
+        <Route>
+          <div>페이지를 찾을 수 없습니다.</div>
+        </Route>
+      </Switch>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
