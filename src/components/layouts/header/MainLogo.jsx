4f40386a70598e9c598a61e28c095536651b4ff2
qorlgns1 <rlgns0610@gmail.com>
2022-06-16 22:18:00 +0900

    feat: MainLogo 컴포넌트 개발

---
+++ src/components/layouts/header/MainLogo.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../../assets/Logo.svg";
+import styled, { createGlobalStyle } from "styled-components";
+
+const MainLogoGlobalStyle = createGlobalStyle`
+  h1 {
+    font-size: 3rem;
+  }
+
+  img {
+    display: block;
+  }
+`;
+
+const HomeLink = styled(Link)`
+  display: block;
+  padding: 0.4rem;
+  margin-left: -0.4rem;
+`;
+
+export default function MainLogo() {
+  return (
+    <>
+      <MainLogoGlobalStyle />
+      <h1>
+        <HomeLink to="/">
+          <img src={logo} alt="My Blog" />
+        </HomeLink>
+      </h1>
+    </>
+  );
+}
